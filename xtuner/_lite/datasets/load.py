b7c200cf308581594017cb8c35268c1de9b37b84
HIT-cwh <2892770585@qq.com>
2024-07-30 08:30:01 +0000

    support load json-like jsonl files

--- xtuner/_lite/datasets/load.py
+++ xtuner/_lite/datasets/load.py
@@ -26,11 +26,14 @@ def load_json(file):
 
 
 def load_jsonl(file):
-    dset = []
-    with open(file) as f:
-        for line in f:
-            # dset.append(line)
-            dset.append(json.loads(line))
+    try:
+        dset = []
+        with open(file) as f:
+            for line in f:
+                dset.append(json.loads(line))
+    except json.JSONDecodeError:
+        dset = load_json(file)
+
     return dset
 
 
