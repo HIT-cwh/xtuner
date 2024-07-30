8bc2da8d822c430709261dd0243db0fec230c275
HIT-cwh <2892770585@qq.com>
2024-07-30 08:28:55 +0000

    add dsv2 chat template

--- xtuner/_lite/chat/templates/__init__.py
+++ xtuner/_lite/chat/templates/__init__.py
@@ -7,7 +7,13 @@ CHAT_TEMPLATE_MAP = {
         system='<|im_start|>system\n{system}<|im_end|>\n',
         user='<|im_start|>user\n{user}<|im_end|>\n<|im_start|>assistant\n',
         assistant='{assistant}<|im_end|>',
-        stop_words=['<|im_end|>'])
+        stop_words=['<|im_end|>']),
+    'deepseek_v2':
+    HybridChatTemplate(
+        system='{system}\n\n',
+        user='User: {user}\n\nAssistant: ',
+        assistant='{assistant}<｜end▁of▁sentence｜>',
+        stop_words=['<｜end▁of▁sentence｜>']),
 }
 
 __all__ = ['ChatTemplate', 'HybridChatTemplate']
