61d26cdb3d72c0e72a871a59a0c1d12c40ebe43b
HIT-cwh <2892770585@qq.com>
2024-08-14 06:34:54 +0000

    add streaming ds

--- xtuner/_lite/datasets/__init__.py
+++ xtuner/_lite/datasets/__init__.py
@@ -3,6 +3,8 @@ from .format import OPENAI_FORMAT_MAP
 from .llava import (LlavaCollator, LlavaRawDataset, LlavaTokenizedDataset,
                     LlavaTokenizeFunction, SoftPackerForLlava)
 from .load import load_datasets
+from .streaming import (MultiStreamingDataset, PretrainTokenizeFunction,
+                        Streaming, StreamingDataset)
 from .text import (HardPackerForText, SoftPackerForText, TextCollator,
                    TextOnlineTokenizeDataset, TextTokenizedDataset,
                    TextTokenizeFunction)
@@ -11,5 +13,7 @@ __all__ = [
     'OPENAI_FORMAT_MAP', 'LlavaCollator', 'LlavaRawDataset',
     'LlavaTokenizedDataset', 'LlavaTokenizeFunction', 'SoftPackerForLlava',
     'load_datasets', 'HardPackerForText', 'SoftPackerForText', 'TextCollator',
-    'TextOnlineTokenizeDataset', 'TextTokenizedDataset', 'TextTokenizeFunction'
+    'TextOnlineTokenizeDataset', 'TextTokenizedDataset',
+    'TextTokenizeFunction', 'MultiStreamingDataset',
+    'PretrainTokenizeFunction', 'Streaming', 'StreamingDataset'
 ]
